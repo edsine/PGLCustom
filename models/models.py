401706a76ec2edf3a16a20de5b3acde5b89c5e87
Omeiza <omeiza.alabi@edsinetechnologiesltd.com>
2022-02-11 11:55:08 +0100

    add professional qualification and employment history details

--- models/models.py
+++ models/models.py
@@ -59,3 +59,20 @@ class Hr(models.Model):
     emergency_contact_2 = fields.Char()
     emergency_phone = fields.Char()
     
+    class_of_degree = fields.Char()
+    deg_start_date = fields.Date()
+    deg_end_date = fields.Date()
+    
+    prof_name_of_institution = fields.Char()
+    prof_certificate_obtained = fields.Char()
+    prof_class_of_certification = fields.Char()
+    prof_start_date = fields.Date()
+    prof_end_date = fields.Date()
+    
+    emp_history_organization = fields.Char()
+    emp_history_role = fields.Char()
+    emp_history_contact_name = fields.Char()
+    emp_history_phone_number = fields.Char()
+    emp_history_start_date = fields.Date()
+    emp_history_end_date = fields.Date()
+    emp_reason_for_leaving = fields.Char()
